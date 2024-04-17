02ad79bb332dd45a5009e85e367751d1ab6c4974
Yunika-Bajracharya <yunibajraa@gmail.com>
2024-04-17 10:50:59 +0545

    show FAQ questions and most similar one

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [result, setResult] = useState(null);
+  const [faqList, setFaqList] = useState([]);
+  const [darkMode, setDarkMode] = useState(true);
+
+  useEffect(() => {
+    // Fetch FAQ list from the backend on component mount
+    fetchFaqList();
+  }, []);
+
+  const fetchFaqList = async () => {
+    try {
+      const response = await axios.get("http://localhost:8000/faq-data/");
+      setFaqList(response.data);
+    } catch (error) {
+      console.error("Error fetching FAQ list:", error);
+    }
+  };
+
+  const handleSearch = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:8000/faq-search/?query=${encodeURIComponent(query)}`
+      );
+      setResult(response.data);
+    } catch (error) {
+      console.error("Error searching FAQ:", error);
+    }
+  };
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`container ${darkMode ? "dark-mode" : ""}`}>
+      <div className="input-container">
+        <h1>Welcome!</h1>
+
+        <input
+          type="text"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder="Search your query..."
+        />
+        <button className="input-button" onClick={handleSearch}>
+          Search
+        </button>
+      </div>
+      {result && (
+        <div className="faq-item">
+          <h2>Most Similar Question & Answer</h2>
+          <h3>Question:</h3>
+          <p>{result.question}</p>
+          <h3>Answer:</h3>
+          <p>{result.answer}</p>
+          <p>Similarity Score: {result.score}</p>
+        </div>
+      )}
+      <div className="faq-list">
+        <h2>Frequently Asked Questions</h2>
+        {faqList.map((faq, index) => (
+          <div key={index} className="faq-item">
+            <p className="faq-question">● {faq.question}</p>
+            <p className="faq-answer"> - {faq.answer}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
